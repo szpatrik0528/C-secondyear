9c95c74783e71535ce90479cf917c8a992ed03e3
szpatrik0528 <szpatrik.photoshop@gmail.com>
2023-09-19 08:16:41 +0200

    hidegburkolo

---
+++ B141/Hidegburkolo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hidegburkolo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérlek add meg a szoba szélességét méterbe: ");
+            double szobaSzelesseg = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Kérlek add meg a szoba husszúságát méterbe: ");
+            double szobaHosszusag = double.Parse(Console.ReadLine());
+
+            double alapterulet = szobaSzelesseg * szobaHosszusag;
+            double oldalfalTerulet = 1.5 * (alapterulet);
+            double teljesTerulet = alapterulet + oldalfalTerulet;
+
+            double egyCsomagTerulet = 1.44;
+            double csomagSzukseges = (teljesTerulet / egyCsomagTerulet);
+
+            Console.WriteLine("Teljes felület: " + teljesTerulet + " m2");
+            Console.WriteLine("Szükséges csempék száma: " + Math.Ceiling(csomagSzukseges) + " csomag kell.");
+
+            Console.ReadKey();
+        }
+    }
+}
